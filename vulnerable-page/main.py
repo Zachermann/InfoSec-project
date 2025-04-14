33db5dbb91ff343cfaef604c0542674f2c03b915
Zach <142842209+Zachermann@users.noreply.github.com>
2025-04-14 19:06:54 -0400

    Update main.py

--- vulnerable-page/main.py
+++ vulnerable-page/main.py
@@ -1,7 +1,7 @@
 import sqlite3
 
 # create empty database
-connection = sqlite3.connect("Sandmann.db")
+connection = sqlite3.connect("SandmannDB.db")
 # communicate with the database
 cursor = connection.cursor()
 
