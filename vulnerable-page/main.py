aeb4d041564a31d9002cafb8ad14dafb0e96f81a
Zach <142842209+Zachermann@users.noreply.github.com>
2025-04-14 19:05:58 -0400

    Create main.py

---
+++ vulnerable-page/main.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+# create empty database
+connection = sqlite3.connect("Sandmann.db")
+# communicate with the database
+cursor = connection.cursor()
+
+cursor.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL UNIQUE, password TEXT NOT NULL)")
+
+connection.commit()
+connection.close()
